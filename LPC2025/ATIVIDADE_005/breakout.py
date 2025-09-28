23fec749410c1695798d0247dbff4e1508623933
Adinã Matos <adinanmatosjunior021@gmail.com>
2025-09-27 22:56:33 -0400

    Update breakout.py
    criação da tela

--- LPC2025/ATIVIDADE_005/breakout.py
+++ LPC2025/ATIVIDADE_005/breakout.py
@@ -15,3 +15,12 @@ pygame.display.set_caption("Breakout - PyGame Edition - 2025-09-27")
 # score text
 score_font = pygame.font.Font("assets/PressStart2P.ttf", 44)
 score_text = score_font.render("000", True, COLOR_WHITE, COLOR_BLACK)
+
+# main loop create
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # quit
+            running = False
+
+pygame.quit()
