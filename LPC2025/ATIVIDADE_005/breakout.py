108b6b1259ef41a051890ccee961014bd97f734a
Josafa Lucena Menezes <jlmenezes.pro@gmail.com>
2025-09-27 22:00:03 -0400

    começo do codigo

---
+++ LPC2025/ATIVIDADE_005/breakout.py
@@ -0,0 +1,17 @@
+import pygame
+import math
+
+pygame.init()
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+
+SCORE_MAX = 4
+
+size = (1280, 720)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout - PyGame Edition - 2025-09-27")
+
+# score text
+score_font = pygame.font.Font("assets/PressStart2P.ttf", 44)
+score_text = score_font.render("000", True, COLOR_WHITE, COLOR_BLACK)
